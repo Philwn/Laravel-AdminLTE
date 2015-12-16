496717a8416a19e3b976e8b430b326ed6600beae
Phil Newell <phil@philnewell.co.uk>
2015-12-16 15:57:36 +0000

    Added Migrations for Images/posts/Tags Tables

---
+++ database/migrations/2015_12_16_153623_create_tags_and_taggables_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTagsAndTaggablesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tags', function (Blueprint $table) {
+            $table->increments('id')->unique();
+            $table->string('name');
+        });
+        
+        Schema::create('taggables', function (Blueprint $table) {
+            $table->integer('tag_id');
+            $table->integer('taggable_id');
+            $table->string('taggable_type');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('tags');
+        Schema::drop('taggables');
+    }
+}
---
+++ database/migrations/2015_12_16_153731_create_posts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id')->unique();
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->text('body');
+            $table->string('display');
+            $table->string('excerpt');
+            $table->string('image_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('posts');
+    }
+}
