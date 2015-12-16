741016c2924a0ca6f71cb4facb30ae50b42064ae
Phil Newell <phil@philnewell.co.uk>
2015-12-16 12:36:07 +0000

    Laravel Auth Library sorted with Login and Register forms

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -24,7 +24,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
         apply the skin class to the body tag so the changes take effect.
   -->
   <link rel="stylesheet" href="{{ asset('bower_components/admin-lte/dist/css/skins/skin-blue.min.css') }}">
-  
+  @yield('styles')
 
 
   <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
